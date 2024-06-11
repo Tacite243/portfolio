f0ef021185190df38ba7d25e2e81d7b1df5093bc
Tacite243 <taciteiragi@gmail.com>
2024-06-11 11:15:27 +0100

    create profile's view

--- portfolio_project/portfolio/views.py
+++ portfolio_project/portfolio/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Profile
+from .serializers import ProfileSerialiszer
 
-# Create your views here.
+class ProfileView(APIView):
+    def get(self, request):
+        profile = Profile.objects.first()
+        serializer = ProfileSerialiszer(profile)
+        return Response(serializer.data)
