6cb2ed1c995ddc2d02117f6d6d4d218618550f60
Tacite243 <taciteiragi@gmail.com>
2024-06-11 11:02:25 +0100

    create profile's model

--- portfolio_project/portfolio/models.py
+++ portfolio_project/portfolio/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+    skills = models.TextField()
+    experience = models.TextField()
+    contact = models.EmailField()
