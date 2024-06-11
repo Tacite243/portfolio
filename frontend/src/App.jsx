9b9ece7f550428cb704f808d6e501184b25c76ec
Tacite243 <taciteiragi@gmail.com>
2024-06-11 12:30:36 +0100

    create profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,35 @@
+import React, { useEffect, useState } from 'react';
 import './index.css'
+import axios from 'axios'
 
 function App() {
+  const [profile, setProfile] = useState(null);
+  
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/profile/').then(response => {
+      setProfile(response.data)
+    }).catch(error => {
+      console.error('Il y a une erreur!', error)
+    });
+  }, []);
+
+  if (!profile) return <div>Chargement...</div>;
+  
   return (
-    <></>
+    <>
+      <div className="app">
+        <header className="App-header">
+          <h1>{profile.name}</h1>
+          <p>{profile.bio}</p>
+          <h2>{Compétences}</h2>
+          <p>{profile.skills}</p>
+          <h2>{Expérience}</h2>
+          <p>{profile.experience}</p>
+          <h2>Contact</h2>
+          <p>{profile.contact}</p>
+        </header>
+      </div>
+    </>
   )
 }
 
